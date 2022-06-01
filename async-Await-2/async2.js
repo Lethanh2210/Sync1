166e13800787a0e6b6ebd72b2ab7d1e5ff24bf41
LeThanh <kzxlmdtn888@gmail.com>
2022-06-01 15:15:21 +0700

    ..

---
+++ async-Await-2/async2.js
@@ -0,0 +1,15 @@
+async function getDivision(a, b) {
+    if (b!==0) return(a/b);
+    return new Error("Math error");
+}
+
+async function f() {
+    try{
+        let result = await getDivision(2, 0);
+        console.log(result)
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
+f();
